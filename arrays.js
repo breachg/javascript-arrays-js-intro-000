5a714637672cc122d4a4ac51880671aa1137f004
Garrett Breach <breachg1992@gmail.com>
2020-10-22 17:39:31 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray (chocolateBars, element) {
+ chocolateBars.unshift("twix")
+   return chocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray () {
+ chocolateBars.unshift("twix")
+   return chocolateBars
+}
