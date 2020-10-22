8b22ab02af85126d5f69dc5e4d7d2625728aabfd
Garrett Breach <breachg1992@gmail.com>
2020-10-22 20:03:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,8 @@ function removeElementFromBeginningOfArray (array) {
    array.pop()
    return array
  }
+
+function removeElementFromEndOfArray (array) {
+  array.slice(0, array.length -1)
+  return array
+}
