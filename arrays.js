77b9085653d36358806045d8b1423fdc093a75c4
Garrett Breach <breachg1992@gmail.com>
2020-10-22 18:49:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 var array = chocolateBars
 var element = 'twix'
 
-
 function addElementToBeginningOfArray (array, element) {
  array = [element, ...array]
    return array
