f02b928b677ae6eff9c0a387227e441fa2a97260
Garrett Breach <breachg1992@gmail.com>
2020-10-22 17:54:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray () {
+function addElementToBeginningOfArray (array, element) {
  chocolateBars = ['twix', ...chocolateBars]
    return chocolateBars
 }
 
-function destructivelyAddElementToBeginningOfArray () {
+function destructivelyAddElementToBeginningOfArray (array, element) {
  chocolateBars.unshift("twix")
    return chocolateBars
 }
